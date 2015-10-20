4cb4c778c4b29a2692d9ef1f5bc69b6a011dc4fe
Zalmoxix <albertosanchezcruz2@gmail.com>
2015-10-20 17:28:13 +0200

    Vistas genericas

--- registro/models.py
+++ registro/models.py
@@ -14,12 +14,7 @@ class Cliente(models.Model):
     def __str__(self):
         return self.cliente_name
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
 
 class Incidencia(models.Model):
     incidencia_cliente = models.ForeignKey(Cliente)
@@ -33,6 +28,12 @@ class Incidencia(models.Model):
     incidencia_clases =models.CharField(max_length=10, choices=incidencia_clase_tipos, default=PREGUNTA)
     incidencia_resuelta = models.BooleanField(default=False)
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
     def __str__(self):
-        return str(self.id) #+ '->   ' + self.incidencia_descripcion[:30]
+        return str(self.id)
 
--- registro/views.py
+++ registro/views.py
@@ -1,16 +1,43 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 
 from .models import Incidencia
 
 
-def index(request):
-    ultimas_incidencias = Incidencia.objects.order_by('-incidencia_fecha')[:5]
-    context = {'ultimas_incidencias': ultimas_incidencias}
-    return render(request, 'registro/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'registro/index.html'
+    context_object_name = 'ultimas_incidencias'
 
-def detail(request, inci_id):
-    inci = get_object_or_404(Incidencia, pk=inci_id)
-    return render(request, 'registro/detail.html', {'inci': inci})
+    def get_queryset(self):
+        return Incidencia.objects.order_by('-incidencia_fecha')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Incidencia
+    template_name = 'registro/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Incidencia
+    template_name = 'registro/results.html'
+
+#def vote(request, question_id):
+#    p = get_object_or_404(Question, pk=question_id)
+#   try:
+#        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+#    except (KeyError, Choice.DoesNotExist):
+#        # Redisplay the question voting form.
+#        return render(request, 'polls/detail.html', {
+#            'question': p,
+#            'error_message': "You didn't select a choice.",
+#        })
+#    else:
+#        selected_choice.votes += 1
+#        selected_choice.save()
+#        # Always return an HttpResponseRedirect after successfully dealing
+#        # with POST data. This prevents data from being posted twice if a
+#        # user hits the Back button.
+#        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
